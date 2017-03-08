d25b1274688838e5f58980b5530d6b769cd22b1f
NsikakApp <nsikakthompson73@gmail.com>
2017-03-08 21:19:26 +0100

    Added: Made some code Refatoring

--- app/src/main/java/ng/codeimpact/sunshineapp/MainActivityFragment.java
+++ app/src/main/java/ng/codeimpact/sunshineapp/MainActivityFragment.java
@@ -1,14 +1,24 @@
 package ng.codeimpact.sunshineapp;
 
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.text.format.Time;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -17,9 +27,12 @@ import java.io.InputStreamReader;
 import java.lang.reflect.Array;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -31,29 +44,20 @@ public class  MainActivityFragment extends Fragment {
     public MainActivityFragment() {
     }
 
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setHasOptionsMenu(true);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
        View rootView =  inflater.inflate(R.layout.fragment_main, container, false);
 
 
-        //List of Dummy weather data
-
-        String[] forecastArray = {
-                "Today - Sunny - 88/63",
-                "Tomorrow - Foogy - 70/40",
-                "Weds - Cloudy - 72/65",
-                "Thurs - Asteroids - 75/65",
-                "Fri - Heavy Rain - 65/65",
-                "Sat - HELP TRAPPED IN WEATHERSTATION - 60/51",
-                "Sun - Sunny - 80/68"
-
-        };
-
-
-        //Array list
-        List<String>  weekForecast = new ArrayList<String>(
-                Arrays.asList(forecastArray));
 
 
         mForecastAdapter = new ArrayAdapter<String>(
@@ -64,7 +68,7 @@ public class  MainActivityFragment extends Fragment {
                 //ID of the textview to populate
                 R.id.list_item_forecast_textview,
                 //Forecast data
-                weekForecast);
+                new ArrayList<String>());
 
         //Get reference to the listview
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
@@ -79,18 +83,103 @@ public class  MainActivityFragment extends Fragment {
 
     }
 
-    public class FetchWeatherTask extends AsyncTask<Void, Void, Void>{
+    public class FetchWeatherTask extends AsyncTask<String, Void, String[]>{
 
         private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
+
+
+
+        private String getReadableDateString(long time){
+            SimpleDateFormat shortenedDateFormat = new SimpleDateFormat("EEE MMM dd");
+            return shortenedDateFormat.format(time);
+        }
+
+        private String formatHighLows(double high, double low) {
+            long roundedHigh = Math.round(high);
+            long roundedLow = Math.round(low);
+
+            String highLowStr = roundedHigh + "/" + roundedLow;
+            return highLowStr;
+        }
+
+        private String[] getWeatherDataFromJson(String forecastJsonStr, int numDays)
+                throws JSONException {
+
+            final String OWM_LIST = "list";
+            final String OWM_WEATHER = "weather";
+            final String OWM_TEMPERATURE = "temp";
+            final String OWM_MAX = "max";
+            final String OWM_MIN = "min";
+            final String OWM_DESCRIPTION = "main";
+
+            JSONObject forecastJson = new JSONObject(forecastJsonStr);
+            JSONArray weatherArray = forecastJson.getJSONArray(OWM_LIST);
+
+
+            Time dayTime = new Time();
+            dayTime.setToNow();
+
+            int julianStartDay = Time.getJulianDay(System.currentTimeMillis(), dayTime.gmtoff);
+
+            dayTime = new Time();
+
+            String[] resultStrs = new String[numDays];
+            for(int i = 0; i < weatherArray.length(); i++) {
+                String day;
+                String description;
+                String highAndLow;
+
+                JSONObject dayForecast = weatherArray.getJSONObject(i);
+
+                long dateTime;
+
+                dateTime = dayTime.setJulianDay(julianStartDay+i);
+                day = getReadableDateString(dateTime);
+
+                JSONObject weatherObject = dayForecast.getJSONArray(OWM_WEATHER).getJSONObject(0);
+                description = weatherObject.getString(OWM_DESCRIPTION);
+
+                JSONObject temperatureObject = dayForecast.getJSONObject(OWM_TEMPERATURE);
+                double high = temperatureObject.getDouble(OWM_MAX);
+                double low = temperatureObject.getDouble(OWM_MIN);
+
+                highAndLow = formatHighLows(high, low);
+                resultStrs[i] = day + " - " + description + " - " + highAndLow;
+            }
+
+            for (String s : resultStrs) {
+                Log.v(LOG_TAG, "Forecast entry: " + s);
+            }
+            return resultStrs;
+        }
         @Override
-        protected Void doInBackground(Void... params) {
+        protected String[] doInBackground(String... params) {
+
 
             HttpURLConnection urlConnection = null;
             BufferedReader reader = null;
 
             String forecastJsonStr = null;
+
+            String format = "json";
+            String units = "metric";
+            int numDays = 7;
             try {
-                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast?q=Uyo,DE&appid=12b713670eb1a25757bef50806438abe");
+
+                final String FORECAST_BASE_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?";
+                final String QUERY_PARAM = "q";
+                final String FORMAT_PARAM = "mode";
+                final String UNITS_PARAM = "units";
+                final String DAYS_PARAM = "cnt";
+
+                Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
+                        .appendQueryParameter(QUERY_PARAM, params[0])
+                        .appendQueryParameter(FORMAT_PARAM, format)
+                        .appendQueryParameter(UNITS_PARAM, units)
+                        .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
+                        .build();
+
+                URL url = new URL(builtUri.toString());
 
                 // Create the request to OpenWeatherMap, and open the connection
                 urlConnection = (HttpURLConnection) url.openConnection();
@@ -102,7 +191,7 @@ public class  MainActivityFragment extends Fragment {
                 StringBuffer buffer = new StringBuffer();
                 if (inputStream == null) {
                     // Nothing to do.
-                    return null;
+                    forecastJsonStr = null;
                 }
                 reader = new BufferedReader(new InputStreamReader(inputStream));
 
@@ -112,13 +201,13 @@ public class  MainActivityFragment extends Fragment {
                 }
 
                 if (buffer.length() == 0) {
-                    return null;
+                    forecastJsonStr = null;
                 }
 
                 forecastJsonStr = buffer.toString();
             } catch (IOException e) {
                 Log.e(LOG_TAG, "Error ", e);
-                return null;
+                forecastJsonStr = null;
             } finally{
                 if (urlConnection != null) {
                     urlConnection.disconnect();
@@ -132,7 +221,40 @@ public class  MainActivityFragment extends Fragment {
                 }
             }
 
+            try{
+                return  getWeatherDataFromJson(forecastJsonStr,numDays);
+            } catch (JSONException e){
+                Log.e(LOG_TAG, e.getMessage());
+                e.printStackTrace();
+            }
             return null;
         }
+
+        @Override
+        protected void onPostExecute(String [] result) {
+            if(result != null){
+                mForecastAdapter.clear();
+                for(String dayForcatStr : result){
+                    mForecastAdapter.add(dayForcatStr);
+                }
+            }
+
+        }
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+       inflater.inflate(R.menu.menu_main, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+       int id = item.getItemId();
+        if(id == R.id.action_refresh){
+            FetchWeatherTask weatherTask = new FetchWeatherTask();
+            weatherTask.execute("uyo");
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
