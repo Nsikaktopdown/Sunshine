66234b574e62d7cff4eb4507fe399480f40f1e79
NsikakApp <nsikakthompson73@gmail.com>
2017-03-08 14:26:38 +0100

    Added: Nework Request code

--- app/src/main/java/ng/codeimpact/sunshineapp/MainActivityFragment.java
+++ app/src/main/java/ng/codeimpact/sunshineapp/MainActivityFragment.java
@@ -1,13 +1,22 @@
 package ng.codeimpact.sunshineapp;
 
+import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.lang.reflect.Array;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -53,11 +62,77 @@ public class  MainActivityFragment extends Fragment {
                 //ID of the List Item layout
                 R.layout.list_item_forecast,
                 //ID of the textview to populate
-                R.id.listview_forecast,
+                R.id.list_item_forecast_textview,
                 //Forecast data
                 weekForecast);
 
-        //Giv
-            return  rootView;
+        //Get reference to the listview
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(mForecastAdapter);
+
+
+
+
+              return  rootView;
+
+
+
+    }
+
+    public class FetchWeatherTask extends AsyncTask<Void, Void, Void>{
+
+        private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            String forecastJsonStr = null;
+            try {
+                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast?q=Uyo,DE&appid=12b713670eb1a25757bef50806438abe");
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    return null;
+                }
+
+                forecastJsonStr = buffer.toString();
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                return null;
+            } finally{
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("MainActivityFragment", "Error closing stream", e);
+                    }
+                }
+            }
+
+            return null;
+        }
     }
 }
